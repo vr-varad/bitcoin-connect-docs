de2890747e926ab5f8cf5c1322b3330f29a89773
vr-varad <varadgupta21@gmail.com>
2024-03-12 10:54:06 +0530

    fixes: fixed the logo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -51,12 +51,11 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
       navbar: {
         title: 'Bitcoin-Connect',
         logo: {
           alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          src: 'img/docusaurus.png',
         },
         items: [
           {
